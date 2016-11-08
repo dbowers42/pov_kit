8a675b807d9e3341b942706fda9821c663cfdd7f
David Bowers <dbowers.github@gmail.com>
2016-11-07 21:41:56 -0700

    dry up template rendering

--- lib/pov_kit/generator.rb
+++ lib/pov_kit/generator.rb
@@ -4,16 +4,31 @@ require 'ostruct'
 module PovKit
   class Generator
     def camera(*options)
+      assigns = {name: options.first}
 
-      template_path = File.dirname(__FILE__) + '/templates/camera.erb.inc'
-      template = File.read(template_path)
-      config = {camera_name: options.first}
-      assigns = OpenStruct.new(config)
-      p ERB.new(template).result(assigns.instance_eval { binding })
+      p render(:camera, assigns)
+    end
+
+    def material(*options)
+      assigns = {name: options.first}
+
+      p render(:material, assigns)
+    end
+
+    def light_source(*_options)
+
     end
 
     def macro(*_options)
 
+    end
+
+    private
+
+    def render(asset, assigns)
+      template_path = File.dirname(__FILE__) + "/templates/#{asset}.erb.inc"
+      template = File.read(template_path)
+      ERB.new(template).result(OpenStruct.new(assigns).instance_eval { binding })
     end
   end
 end
