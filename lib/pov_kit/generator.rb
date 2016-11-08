e9815ecb1e6af23f6f50382f18def7c35b736d44
David Bowers <dbowers.github@gmail.com>
2016-11-07 21:18:28 -0700

    move templates

--- lib/pov_kit/generator.rb
+++ lib/pov_kit/generator.rb
@@ -4,7 +4,9 @@ require 'ostruct'
 module PovKit
   class Generator
     def camera(*options)
-      template = File.read('templates/camera.erb.inc')
+
+      template_path = File.dirname(__FILE__) + '/templates/camera.erb.inc'
+      template = File.read(template_path)
       config = {camera_name: options.first}
       assigns = OpenStruct.new(config)
       p ERB.new(template).result(assigns.instance_eval { binding })
