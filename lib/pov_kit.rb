83144edbb535e5949f650519c50e1a041e14fe5c
David Bowers <dbowers.github@gmail.com>
2016-11-07 21:02:41 -0700

    add generator for cameras

--- lib/pov_kit.rb
+++ lib/pov_kit.rb
@@ -1,4 +1,5 @@
 require 'pov_kit/version.rb'
+require 'pov_kit/generator'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
--- lib/pov_kit/generator.rb
+++ lib/pov_kit/generator.rb
@@ -1,3 +1,17 @@
-class Generator
+require 'erb'
+require 'ostruct'
 
+module PovKit
+  class Generator
+    def camera(*options)
+      template = File.read('templates/camera.erb.inc')
+      config = {camera_name: options.first}
+      assigns = OpenStruct.new(config)
+      p ERB.new(template).result(assigns.instance_eval { binding })
+    end
+
+    def macro(*_options)
+
+    end
+  end
 end
